2507949f9cb6ed2107de93610ece372216385195
pcnnadi <psxpn2@nottingham.ac.uk>
2024-02-06 11:20:47 +0000

    lab 2 template and instructions

--- lab2/simpleBot2.py
+++ lab2/simpleBot2.py
@@ -7,31 +7,56 @@ class Brain():
 
     def __init__(self,botp):
         self.bot = botp
-        self.moves = 0  # attribute to estimate distance covered by bot.
-        self.batteryLevel = 100
+        self.turningCount = 0
+        self.movingCount = random.randrange(50,100)
+        self.currentlyTurning = False
 
     # modify this to change the robot's behaviour
-    def thinkAndAct(self, lightL, lightR, x, y, sl, sr):
-        # wheels not moving - no movement - no response to light
-        # total_light = lightL + lightR
-        self.moves = self.moves+1 if self.moves < 90 else 0  # increment distance on each turn. reset to 0 on 100.
-        self.batteryLevel = max(self.batteryLevel - 1, 0) if self.moves % 10 == 0 and self.batteryLevel > 0 else self.batteryLevel
-        # Make the bot turn left and right at random with speed depending on proximity to light source.
-        random_turn = random.randint(1,2)  # randomly select a direction. 1 - left, 2 -right
-        # #  set leftSpeed to 10 if distance is 60 and bot chose left or is already going left and was not going right.
-        # speedLeft = 10 if self.moves > 59 and (random_turn == 1 or sl == 10) and sr != 10 else 3 * total_light
-        # #  set rightSpeed to 10 so bot turns right if it did not choose left at random.
-        # speedRight = 10 if self.moves > 59 and speedLeft != 10 else 3 * total_light
+    def thinkAndAct(self, lightL, lightR, chargerL, chargerR, x, y, sl, sr, battery):
+        newX = None
+        newY = None
+        
+        # wandering behaviour
+        if self.currentlyTurning==True:
+            speedLeft = -2.0
+            speedRight = 2.0
+            self.turningCount -= 1
+        else:
+            speedLeft = 5.0
+            speedRight = 5.0
+            self.movingCount -= 1
+        if self.movingCount==0 and not self.currentlyTurning:
+            self.turningCount = random.randrange(20,40)
+            self.currentlyTurning = True
+        if self.turningCount==0 and self.currentlyTurning:
+            self.movingCount = random.randrange(50,100)
+            self.currentlyTurning = False
 
-        # Make the bot turn towards the light, but it wobbles.
-        # speedLeft = 10 if lightL < lightR and self.moves < 15 else 3
-        # speedRight = 10 if lightR < lightL and self.moves < 15 else 3
+        #battery - these are later so they have priority
+        if battery<600:
+            if chargerR>chargerL:
+                speedLeft = 2.0
+                speedRight = -2.0
+            elif chargerR<chargerL:
+                speedLeft = -2.0
+                speedRight = 2.0
+            if abs(chargerR-chargerL)<chargerL*0.1: #approximately the same
+                speedLeft = 5.0
+                speedRight = 5.0
+        if chargerL+chargerR>200 and battery<1000:
+            speedLeft = 0.0
+            speedRight = 0.0
 
-        # Make the bot run away from the light
-        speedLeft = 10 if lightL > 200 or (self.moves > 79 and (random_turn == 1 or sl == 1) and sr != 10) else 3
-        speedRight = 10 if lightR > 400 or (self.moves > 79 and speedLeft != 10) else 3
-        newX = 1 if x > 999 else 999 if x < 1 else None
-        newY = 1 if y > 999 else 999 if y < 1 else None
+        #toroidal geometry
+        if x>1000:
+            newX = 0
+        if x<0:
+            newX = 1000
+        if y>1000:
+            newY = 0
+        if y<0:
+            newY = 1000
+
         return speedLeft, speedRight, newX, newY
 
 class Bot():
@@ -45,10 +70,13 @@ class Bot():
         self.ll = 60 #axle width
         self.sl = 0.0
         self.sr = 0.0
+        self.battery = 1000
 
     def thinkAndAct(self, agents, passiveObjects):
         lightL, lightR = self.senseLight(passiveObjects)
-        self.sl, self.sr, xx, yy = self.brain.thinkAndAct(lightL, lightR, self.x, self.y, self.sl, self.sr)
+        chargerL, chargerR = self.senseChargers(passiveObjects)
+        self.sl, self.sr, xx, yy = self.brain.thinkAndAct\
+            (lightL, lightR, chargerL, chargerR, self.x, self.y, self.sl, self.sr, self.battery)
         if xx != None:
             self.x = xx
         if yy != None:
@@ -71,12 +99,36 @@ class Bot():
                 lightL += 200000/(distanceL*distanceL)
                 lightR += 200000/(distanceR*distanceR)
         return lightL, lightR
-    
+
+    #returns sensors values that detect chargers
+    def senseChargers(self, passiveObjects):
+        chargerL = 0.0
+        chargerR = 0.0
+        for pp in passiveObjects:
+            if isinstance(pp,Charger):
+                lx,ly = pp.getLocation()
+                distanceL = math.sqrt( (lx-self.sensorPositions[0])*(lx-self.sensorPositions[0]) + \
+                                       (ly-self.sensorPositions[1])*(ly-self.sensorPositions[1]) )
+                distanceR = math.sqrt( (lx-self.sensorPositions[2])*(lx-self.sensorPositions[2]) + \
+                                       (ly-self.sensorPositions[3])*(ly-self.sensorPositions[3]) )
+                chargerL += 200000/(distanceL*distanceL)
+                chargerR += 200000/(distanceR*distanceR)
+        return chargerL, chargerR
+
+    def distanceTo(self,obj):
+        xx,yy = obj.getLocation()
+        return math.sqrt( math.pow(self.x-xx,2) + math.pow(self.y-yy,2) )
 
     # what happens at each timestep
-    def update(self,canvas,dt):
+    def update(self,canvas,passiveObjects,dt):
         # for now, the only thing that changes is that the robot moves
         #   (using the current settings of self.sl and self.sr)
+        self.battery -= 1
+        for rr in passiveObjects:
+            if isinstance(rr,Charger) and self.distanceTo(rr)<80:
+                self.battery += 10
+        if self.battery<=0:
+            self.battery = 0
         self.move(canvas,dt)
 
     # draws the robot at its current position
@@ -100,9 +152,10 @@ class Bot():
     
         centre1PosX = self.x 
         centre1PosY = self.y
-        canvas.create_oval(centre1PosX-8,centre1PosY-8,\
-                           centre1PosX+8,centre1PosY+8,\
+        canvas.create_oval(centre1PosX-16,centre1PosY-16,\
+                           centre1PosX+16,centre1PosY+16,\
                            fill="gold",tags=self.name)
+        canvas.create_text(self.x,self.y,text=str(self.battery),tags=self.name)
 
         wheel1PosX = self.x - 30*math.sin(self.theta)
         wheel1PosY = self.y + 30*math.cos(self.theta)
@@ -130,8 +183,10 @@ class Bot():
     # handles the physics of the movement
     # cf. Dudek and Jenkin, Computational Principles of Mobile Robotics
     def move(self,canvas,dt):
-        if self.brain.batteryLevel == 0:
-            return
+        print(self.battery)
+        if self.battery==0:
+            self.sl = 0
+            self.sl = 0
         if self.sl==self.sr:
             R = 0
         else:
@@ -157,6 +212,17 @@ class Bot():
             self.y += self.sr*math.sin(self.theta)
         canvas.delete(self.name)
         self.draw(canvas)
+
+    def collectDirt(self, canvas, passiveObjects):
+        toDelete = []
+        for idx,rr in enumerate(passiveObjects):
+            if isinstance(rr,Dirt):
+                if self.distanceTo(rr)<30:
+                    canvas.delete(rr.name)
+                    toDelete.append(idx)
+        for ii in sorted(toDelete,reverse=True):
+            del passiveObjects[ii]
+        return passiveObjects
         
 
 class Lamp():
@@ -169,6 +235,48 @@ class Lamp():
         body = canvas.create_oval(self.centreX-10,self.centreY-10, \
                                   self.centreX+10,self.centreY+10, \
                                   fill="yellow",tags=self.name)
+
+    def getLocation(self):
+        return self.centreX, self.centreY
+
+class Charger():
+    def __init__(self,namep):
+        self.centreX = random.randint(100,900)
+        self.centreY = random.randint(100,900)
+        self.name = namep
+        
+    def draw(self,canvas):
+        body = canvas.create_oval(self.centreX-10,self.centreY-10, \
+                                  self.centreX+10,self.centreY+10, \
+                                  fill="red",tags=self.name)
+
+    def getLocation(self):
+        return self.centreX, self.centreY
+
+class WiFiHub:
+    def __init__(self,namep,xp,yp):
+        self.centreX = xp
+        self.centreY = yp
+        self.name = namep
+        
+    def draw(self,canvas):
+        body = canvas.create_oval(self.centreX-10,self.centreY-10, \
+                                  self.centreX+10,self.centreY+10, \
+                                  fill="purple",tags=self.name)
+
+    def getLocation(self):
+        return self.centreX, self.centreY
+
+class Dirt:
+    def __init__(self,namep):
+        self.centreX = random.randint(100,900)
+        self.centreY = random.randint(100,900)
+        self.name = namep
+
+    def draw(self,canvas):
+        body = canvas.create_oval(self.centreX-1,self.centreY-1, \
+                                  self.centreX+1,self.centreY+1, \
+                                  fill="grey",tags=self.name)
 
     def getLocation(self):
         return self.centreX, self.centreY
@@ -186,7 +294,7 @@ def buttonClicked(x,y,agents):
             rr.x = x
             rr.y = y
 
-def createObjects(canvas,noOfBots,noOfLights):
+def createObjects(canvas,noOfBots=2,noOfLights=2,amountOfDirt=300):
     agents = []
     passiveObjects = []
     for i in range(0,noOfBots):
@@ -195,23 +303,41 @@ def createObjects(canvas,noOfBots,noOfLights):
         bot.setBrain(brain)
         agents.append(bot)
         bot.draw(canvas)
+
     for i in range(0,noOfLights):
         lamp = Lamp("Lamp"+str(i))
         passiveObjects.append(lamp)
         lamp.draw(canvas)
+
+    charger = Charger("Charger"+str(i))
+    passiveObjects.append(charger)
+    charger.draw(canvas)
+    
+    hub1 = WiFiHub("Hub1",950,50)
+    passiveObjects.append(hub1)
+    hub1.draw(canvas)
+    hub2 = WiFiHub("Hub2",50,500)
+    passiveObjects.append(hub2)
+    hub2.draw(canvas)
+
+    for i in range(0,amountOfDirt):
+        dirt = Dirt("Dirt"+str(i))
+        passiveObjects.append(dirt)
+        dirt.draw(canvas)
     canvas.bind( "<Button-1>", lambda event: buttonClicked(event.x,event.y,agents) )
     return agents, passiveObjects
 
 def moveIt(canvas,agents,passiveObjects):
     for rr in agents:
         rr.thinkAndAct(agents,passiveObjects)
-        rr.update(canvas,1.0)
+        rr.update(canvas,passiveObjects,1.0)
+        passiveObjects = rr.collectDirt(canvas,passiveObjects)
     canvas.after(50,moveIt,canvas,agents,passiveObjects)
 
 def main():
     window = tk.Tk()
     canvas = initialise(window)
-    agents, passiveObjects = createObjects(canvas,noOfBots=1,noOfLights=1)
+    agents, passiveObjects = createObjects(canvas,noOfBots=2,noOfLights=0,amountOfDirt=300)
     moveIt(canvas,agents,passiveObjects)
     window.mainloop()
 
