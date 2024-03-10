c74a41b87f91be3aecd3e73ff7a81cd9b406283c
pcnnadi <psxpn2@nottingham.ac.uk>
2024-03-10 13:27:13 +0000

    Task 1 - processing some facts

--- lab 5/chatbot.py
+++ lab 5/chatbot.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+import random
 
 knowledge = { ("person1", "name", "?"), \
               ("person1", "town", "?"),
@@ -13,9 +14,12 @@ while active:
     print("KNOWN:")
     pprint(knowledge - unknowns)
     if unknowns: #is non-empty
-        question = "## to fill in ## "
+        currentQuery = random.choice(list(unknowns))  # choose an item from unknowns at random
+        question = "What is your " + currentQuery[1] + ": "  # set question to ask about a factt.
+        knowledge.remove(currentQuery)  # remove from what we do not know since we are about to find out.
         reply = input(question)
-        # to fill in - process reply
+        # to fill in - process replys
+        knowledge.add((currentQuery[0], currentQuery[1], reply))  # update what we know with the value in reply.
     else:
         question = "How can I help you? "
         helpRequest = input(question)
