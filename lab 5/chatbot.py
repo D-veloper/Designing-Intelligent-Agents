c8bc6bc404f7b6608e983ada943d4d84749ecb70
pcnnadi <psxpn2@nottingham.ac.uk>
2024-03-10 13:44:52 +0000

    Task 2 - processing replies slightly better

--- lab 5/chatbot.py
+++ lab 5/chatbot.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 import random
+import nltk
 
 knowledge = { ("person1", "name", "?"), \
               ("person1", "town", "?"),
@@ -19,7 +20,14 @@ while active:
         knowledge.remove(currentQuery)  # remove from what we do not know since we are about to find out.
         reply = input(question)
         # to fill in - process replys
-        knowledge.add((currentQuery[0], currentQuery[1], reply))  # update what we know with the value in reply.
+        # knowledge.add((currentQuery[0], currentQuery[1], reply))  # update what we know with the value in reply.
+        # Prev processing was crude. What if reply is a sentence, for instance? Therefore, it needs further processing.
+        listOfWords = nltk.word_tokenize(reply)  # break down reply into tokens
+        taggedListOfWords = nltk.pos_tag(listOfWords)  # returns tuples with words and grammatical functions
+        for taggedWord in taggedListOfWords:
+            if taggedWord[1] == "NNP":  # if the word is tagged as a proper noun.
+                knowledge.add((currentQuery[0], currentQuery[1], taggedWord[0]))  # Now the bot is a little smarter.
+
     else:
         question = "How can I help you? "
         helpRequest = input(question)
